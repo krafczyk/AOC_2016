335a7d9084416f84c6665ba2c4019877c423da7a
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-06 17:54:39 -0500

    Test basic memory allocation

--- src/day_1.s
+++ src/day_1.s
@@ -1,16 +1,27 @@
     .section .rodata
     .text
-hello:
-    .string "Hello World!\n"
+map_failed:
+    .string "Map failed!\n"
+map_succeeded:
+    .string "Map succeeded!\n"
 
     .global _start
     .text
 _start:
-    leaq hello(%rip), %rdi
+    movq $10, %rdi
+    call mem_alloc
+
+    cmpq $0,%rax
+    jne mem_success
+    leaq map_failed(%rip), %rdi
+    call printcstr
+
+mem_success:
+
+    leaq map_succeeded(%rip), %rdi
     call printcstr
 
     // exit sequence
     movq $60,%rax; // Syscall 60 is exit
     xorq %rdi,%rdi; // Set exit value
     syscall; // Make the syscall
-
