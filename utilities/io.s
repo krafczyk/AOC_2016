cab0a832fda020fc8df69f1b12adf3da55202770
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-09 11:35:14 -0500

    Finish implementation of uintstr

--- utilities/io.s
+++ utilities/io.s
@@ -1,3 +1,8 @@
+    .text
+    .section .rodata
+    .globl ioendl
+ioendl:
+    .string "\n"
 // Get the length of a zero-terminated string
 // %rdi - string
 // returns - %rax - length of string
@@ -50,35 +55,47 @@ printcstr:
     .globl uintstr
     // Find 'length' necessary for the string
 uintstr:
-    movq $1, %rsi
+    movq $0, %rsi
     movq %rdi, %rax
     movq $10, %rcx
-    jp uintstrbeg
 uintstrbegloop:
     incq %rsi
-uintstrbeg:
     movq $0, %rdx
     div %rcx
     cmpq $0,%rax
     jne uintstrbegloop
-    addq $1, %rsi
-    movq %rsi, %rcx
+    incq %rsi; // %rsi now contains the length
 
     // Allocate memory
-    movq %rcx, %rdi
+    pushq %rdi
+    movq %rsi, %rdi
+    pushq %rsi
     call mem_alloc
+    popq %rsi
+    popq %rdi
 
-    cmp $0,%rax
+    cmpq $0,%rax
     jne uintstr0
     ret
 
 uintstr0:
     // Now we need to set the string.
-
-
-
-
-
+    // Save initial variable
+    movq %rax,%r9; // The address of the new string
+    movq %rdi,%rax; // Save the input argument as numerator
+    decq %rsi; // decrease since we can't access beyond the array
+    movb $0,(%r9,%rsi); // Set the 0 byte at the end of string.
+    movq $10,%rcx; // Dividend
+uintstr1:
+    decq %rsi; // Decrease by one.
+    movq $0,%rdx; // Zero out rdx
+    div %rcx; // Divide numerator
+    addq $48,%rdx; // Add ascii value for 0 to remainder.
+    movb %dl,(%r9,%rsi); // Save string value at byte
+    cmpq $0,%rsi
+    jne uintstr1
+    movq %r9,%rax; // Save string to return
+    ret
 //    .text
 //    .globl open
 // open a file and get a file descriptor
