8546f8b3d55eb69d059951ecea39d06803981e9a
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-09 15:36:38 -0500

    Update testbed

--- src/day_1.s
+++ src/day_1.s
@@ -7,8 +7,30 @@ map_succeeded:
     .global _start
     .text
 _start:
-    movq $2000212,%rdi
+    movq $2120,%rdi
+    pushq %rdi
     call uintstr
+
+    movq %rax,%rdi
+    call cstrlen
+    movq %rax,%rsi
+
+    pushq %rdi
+    pushq %rsi
+
+    call printcstr
+
+    leaq ioendl(%rip),%rdi
+    movq $2,%rsi
+    call printcstr
+
+    popq %rsi
+    popq %rdi
+
+    call mem_free
+
+    popq %rdi
+    call uinthexstr
 
     movq %rax,%rdi
     call cstrlen
