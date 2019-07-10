d3221aa54eaec9f54ca7e002e290b8a09c928c60
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-10 12:12:14 -0500

    Update testbed

--- src/day_1.s
+++ src/day_1.s
@@ -1,26 +1,45 @@
     .section .rodata
     .text
-map_failed0:
-    .string "Map failed!\n"
-map_succeeded:
-    .string "Map succeeded!\n"
+str0:
+    .string "test0"
+str1:
+    .string "test0"
+str2:
+    .string "test0"
+str3:
+    .string "tes"
+mess1:
+    .string "str0 and str1 are equal!\n"
+mess2:
+    .string "str0 and str1 are not equal..\n"
+mess3:
+    .string "str2 and str3 are equal..\n"
+mess4:
+    .string "str2 and str3 are not equal!\n"
     .global _start
     .text
 _start:
-    movq $-9090909,%rdi
-    call intstr
-
-    movq %rax,%rdi
+    movq $str0,%rdi
+    movq $str1,%rsi
+    call cstrcmp
+    movq $mess1,%rdi
+    movq $mess1,%rbx
+    cmpq $0,%rax
+    cmovneq %rbx,%rdi
     call cstrlen
-
     movq %rax,%rsi
-    pushq %rsi
-    pushq %rdi
     call printcstr
-    popq %rdi
-    popq %rsi
 
-    call mem_free
+    movq $str2,%rdi
+    movq $str3,%rsi
+    call cstrcmp
+    movq $mess3,%rdi
+    movq $mess4,%rbx
+    cmpq $0,%rax
+    cmovneq %rbx,%rdi
+    call cstrlen
+    movq %rax,%rsi
+    call printcstr
 
     // exit sequence
     movq $60,%rax; // Syscall 60 is exit
