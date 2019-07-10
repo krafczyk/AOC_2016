5048e057c7e0e0fa0d722482d2ce4c2cfb2f9545
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-10 15:42:01 -0500

    Update testbed for testing argument functions

--- src/day_1.s
+++ src/day_1.s
@@ -1,41 +1,21 @@
     .section .rodata
     .text
-str0:
-    .string " arguments\n"
+inputid:
+    .string "-i"
     .global _start
     .text
 _start:
-    movq (%rsp),%r15; // Save the number of arguments: argc
-    movq %r15,%rdi
-    call uintstr
+    movq %rsp,%rdi
+    movq $inputid,%rsi
+    call getargid
+    cmpq $0,%rax
+    je ex
     movq %rax,%rdi
     call cstrlen
     movq %rax,%rsi
-    pushq %rdi
-    pushq %rsi
-    call printcstr
-    popq %rsi
-    popq %rdi
-    call mem_free
-    movq $str0,%rdi
-    call cstrlen
-    movq %rax,%rsi
     call printcstr
 
-    movq $0,%r14; // Start loop counter
-l0:
-    incq %r14
-    movq (%rsp,%r14,8),%rdi
-    call cstrlen
-    movq %rax,%rsi
-    call printcstr
-    movq $ioendl,%rdi
-    call cstrlen
-    movq %rax,%rsi
-    call printcstr
-    cmp %r15,%r14
-    jne l0
-
+ex:
     // exit sequence
     movq $60,%rax; // Syscall 60 is exit
     xorq %rdi,%rdi; // Set exit value
